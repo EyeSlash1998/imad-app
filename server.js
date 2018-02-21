94ee159818e853e0220f91f5bcc3729bc8a6a3ff
EyeSlash1998 <36436501+EyeSlash1998@users.noreply.github.com>
2018-02-21 17:59:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,19 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/article-one', function (req, res){
+   res.send('Article One Will be Requested and Serverd Here.') 
+});
+
+app.get('/article-two', function (req, res){
+   res.send('Article Two Will be Requested and Serverd Here.') 
+});
+
+app.get('/article-three',twonction (req, res){
+   res.send('Article Three be Requested and Serverd Here.') 
+});
+
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
