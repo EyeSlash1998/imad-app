5ee7267269e00ac2d368a8c900de338dd3b0efda
EyeSlash1998 <36436501+EyeSlash1998@users.noreply.github.com>
2018-02-21 19:44:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+
+
 var articles = {
     'article-one' : {
     title:  'Article One | Rohan Jadhav',
@@ -87,8 +93,6 @@ function createTemplate (data) {
       `;
       return htmlTemplate;
 }
-
-
 app.get('/:articleName', function (req, res){
     var articleName=req.params.articleName;
    res.send(createTemplate(articles[articleName])); 
