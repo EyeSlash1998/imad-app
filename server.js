9c1b959803bed34205e1a9d264c1b50ab8d516b3
EyeSlash1998 <36436501+EyeSlash1998@users.noreply.github.com>
2018-02-21 19:33:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ function createTemplate (data) {
       return htmlTemplate;
 }
 
-app.get('/', function (req, res) {
+app.get('//ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
