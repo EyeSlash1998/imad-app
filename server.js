67f4632bd39c8473c90bafeb589eac05ea20f01c
EyeSlash1998 <36436501+EyeSlash1998@users.noreply.github.com>
2018-02-23 23:07:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.get('/counter', function (req,res) {
 });
 
 var names=[];
-app.get('/submit-name', function (req,res) {
+app.get('/submit-name/', function (req,res) {
     var name=req.query.name;
     
     names.push(name);
