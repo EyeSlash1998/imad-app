723a9a7f28d2c939998f314440ba289956fa2f5d
EyeSlash1998 <36436501+EyeSlash1998@users.noreply.github.com>
2018-02-23 21:08:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,6 +13,7 @@ button.onclick= function() {
             if(request.status===200) {
                 var counter = request.responseText;
                 var span=document.getElementById('count');
+                count++;
                 span.innerHTML=counter.toString();
             }
         }
