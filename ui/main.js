7d4b1775781b2b8ddc3382126bcf52d6bada6bc2
EyeSlash1998 <36436501+EyeSlash1998@users.noreply.github.com>
2018-02-23 22:56:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,13 +23,13 @@ button.onclick= function() {
     request.send(null);
 };
 //submit name
-var nameInp=document.getElementById('name');
-var namel=nameInp.value;
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick= function() {
   var names=['name1', 'name2' ,'name3', 'name4'];
   var list='';
-  for(var i=0;i< name.length;i++)
+  for(var i=0;i< names.length;i++)
       list+='<li>'+names[i]+'</li>';
   };
   var ul=document.getElementById('namelist');
